c3daf4489c81101457daaaf5e62fc43c813f83b2
Tucker Beck <tbeck@comscore.com>
2018-05-09 21:19:55 -0700

    Issue #12: Fixed long_description

--- .project_metadata.py
+++ .project_metadata.py
@@ -3,6 +3,7 @@
     'author_email': 'tucker.beck@gmail.com',
     'copyright': '2017, Tucker Beck',
     'description': 'Extra bindings on py-buzz specifically for flask apps',
+    'url': 'https://github.com/dusktreader/flask-buzz',
     'license': 'MIT',
     'name': 'flask-buzz',
     'release': '0.1.4',
--- setup.py
+++ setup.py
@@ -8,12 +8,20 @@ with open('.project_metadata.py') as meta_file:
     project_metadata = ast.literal_eval(meta_file.read())
 
 
+with open('README.rst') as readme_file:
+    long_description = readme_file.read()
+    long_description_content_type = 'text/x-rst'
+
+
 setup(
     name=project_metadata['name'],
     version=project_metadata['release'],
     author=project_metadata['author'],
     author_email=project_metadata['author_email'],
     description=project_metadata['description'],
+    url=project_metadata['url'],
+    long_description=long_description,
+    long_description_content_type=long_description_content_type,
     license=project_metadata['license'],
     install_requires=[
         'py-buzz',
