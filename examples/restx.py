52f37a36d7b4358125e7f8df2ccd6806fc34c346
Volker Austrup <volker.andreas.austrup@cern.ch>
2021-06-16 12:15:29 +0200

    Migrate from Flask RestPlus to Flask RestX

--- examples/restx.py
+++ examples/restx.py
@@ -1,14 +1,14 @@
 import flask
 import flask_buzz
-import flask_restplus
+import flask_restx
 
 app = flask.app.Flask(__name__)
-api = flask_restplus.Api(app)
-flask_buzz.FlaskBuzz.register_error_handler_with_flask_restplus(api)
+api = flask_restx.Api(app)
+flask_buzz.FlaskBuzz.register_error_handler_with_flask_restx(api)
 
 
 @api.route('/index')
-class MyResource(flask_restplus.Resource):
+class MyResource(flask_restx.Resource):
 
     def get(self):
         raise flask_buzz.FlaskBuzz("I died")
